120d7558aded149956ba28323c978beede9de123
Dean Gutknecht <gutknechtwd@appstate.edu>
2024-02-13 15:24:13 -0500

    Exists

--- VisData/src/app/error/error.component.spec.ts
+++ VisData/src/app/error/error.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ErrorComponent } from './teapot.component';
+import { ErrorComponent } from './error.component';
 
 describe('TeapotComponent', () => {
   let component: ErrorComponent;
