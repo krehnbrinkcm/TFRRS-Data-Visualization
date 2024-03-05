201026f6192bfc31010213bca72264dcff3e24d9
Dean Gutknecht <gutknechtwd@appstate.edu>
2024-03-05 15:41:21 -0500

    Added a seasons service and updated JS imports

---
+++ VisData/src/app/seasons/season.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Observable, from } from "rxjs";
+import * as  scraperFunctions from '../../../../web-scraper/index.js'
+
+export interface seasonObj {
+    season: string,
+    index: number,
+}
+
+
+
+@Injectable({providedIn: 'root',})
+
+export class SeasonService {
+    seasons:Array<seasonObj> = [];
+
+    constructor(){}
+
+    getSeasons(): Observable<any[]> {
+        return from(scraperFunctions.scrapeSeasons());
+    }
+}
