06d610b4d25d848d9814ecf52e5eb0283fa3ade4
ColeKrehnbrink <krehnbrinkcm@gmail.com>
2024-03-05 15:07:50 -0500

    seasons stuff dosent work

--- VisData/src/app/seasons/seasons.component.ts
+++ VisData/src/app/seasons/seasons.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import * as  scraperFunctions from '../../../../web-scraper/index.js'
 
 @Component({
   selector: 'app-seasons',
@@ -6,17 +9,20 @@ import { Component } from '@angular/core';
   styleUrl: './seasons.component.css'
 })
 
-export class SeasonsComponent {
-  seasons = [
-  {season: "2023 Outdoor", id: 0},
-  {season: "2023 Indoor", id: 1},
-  {season: "2022 Outdoor", id: 2},
-  {season: "2022 Indoor", id: 3},
-  {season: "2021 Outdoor", id: 4},
-  {season: "2021 Indoor", id: 5},
-  {season: "2020 Outdoor", id: 6},
-  {season: "2020 Indoor", id: 7},
-  {season: "2019 Outdoor", id: 8},
-  {season: "2019 Indoor", id: 9}
-]
+export class SeasonsComponent{
+
+   seasons = scraperFunctions.scrapeSeasons();
+
+//   seasons = [
+//   {season: "2023 Outdoor", id: 0},
+//   {season: "2023 Indoor", id: 1},
+//   {season: "2022 Outdoor", id: 2},
+//   {season: "2022 Indoor", id: 3},
+//   {season: "2021 Outdoor", id: 4},
+//   {season: "2021 Indoor", id: 5},
+//   {season: "2020 Outdoor", id: 6},
+//   {season: "2020 Indoor", id: 7},
+//   {season: "2019 Outdoor", id: 8},
+//   {season: "2019 Indoor", id: 9}
+// ]
 }
